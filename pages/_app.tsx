f81b03a8efaf3602be2c026096f2c53f08a970a4
choiseongjun <csj2702@naver.com>
2022-01-18 11:14:17 +0900

    global css 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
 import { wrapper } from '../modules/store';
+import {ThemeProvider} from 'styled-components';
+import {theme} from '../src/styles/theme';
+import {GlobalStyle} from '../src/styles/global-styles';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  )
 }
 
 export default wrapper.withRedux(MyApp);
