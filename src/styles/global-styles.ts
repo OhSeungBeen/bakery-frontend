4d27d84fdadeed555d6b2851ffb990816dfd97b0
choiseongjun <csj2702@naver.com>
2022-01-18 15:23:22 +0900

    global style 수정

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -2,17 +2,48 @@ import {createGlobalStyle} from 'styled-components';
 import {normalize} from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
-  ${normalize}
-  html {
-    box-sizing: border-box;
-    font-size: 16px;
-    min-width: 320px;
-  }
-  *,
-  *::before,
-  *::after {
-    box-sizing: inherit;
-  }
-  * { font-family: 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif';}
-  a { cursor: pointer; text-decoration: none; }
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Noto Sans KR", "Noto Sans CJK KR
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+  word-break: keep-all;
+}
+ 
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: inherit;
+}
+input,
+button,
+textarea {
+  font-family: inherit;
+}
+html,
+body,
+#root {
+  height: 100%;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+}
+ul,
+li {
+  list-style-type: none;
+  padding-left: 0px;
+}
+a {
+  text-decoration: none;
+}
+main {
+  padding-top: 70px;
+}
 `;
