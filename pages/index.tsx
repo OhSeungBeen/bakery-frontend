846a1746dc1db16b2b8d1e150cae2c3e65b771ad
hx2ryu <hx2ryu@gmail.com>
2022-03-03 21:46:38 +0900

    feat: add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,15 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import { useState } from 'react';
-import BottomTab from '../components/common/bottomTab';
-import Search from '../components/search/serach';
+import { ReactElement } from 'react';
 import styled from 'styled-components';
 import Home from '../components/home/home';
+import Layout from '@/components/common/Layout';
 
 const HomePage: NextPage = () => {
-  const [tab, setTab] = useState<number>(1);
+  return <Home />;
+};
 
-  const renderTab = () => {
-    switch (tab) {
-      case 1:
-        return <Home />;
-      case 2:
-        return <Search />;
-
-      // some other cases
-
-      default:
-        return <></>;
-    }
-  };
-
-  return (
-    <>
-      <Head>
-        <title>Bakery | Home</title>
-        <meta name="" content="" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* <ContentContainer></ContentContainer> */}
-      {renderTab()}
-      <BottomTab setTab={setTab} />
-    </>
-  );
+HomePage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout withNavigator>{page}</Layout>;
 };
 
 const ContentContainer = styled.div`
