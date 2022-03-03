45e9dc93d39dffddcea265e065a20587daf9313c
hx2ryu <hx2ryu@gmail.com>
2022-03-03 22:07:45 +0900

    feat: implement checkbox

--- components/common/index.ts
+++ components/common/index.ts
@@ -2,5 +2,7 @@ import Seo from './Seo';
 import Link from './Link';
 import MainNavBar from './MainNavBar';
 import Layout from './Layout';
+import Tag from './Tag';
+import CheckBox from './CheckBox';
 
-export { Seo, Link, MainNavBar, Layout };
+export { Seo, Link, MainNavBar, Layout, Tag, CheckBox };
